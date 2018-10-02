794e956317216f9ac2ff68d1d127e226278797b6
bengrenier <43651624+bengrenier@users.noreply.github.com>
2018-10-02 11:30:38 -0700

    its day 2
    we worked on a bunch today

---
+++ hello_again.py
@@ -0,0 +1,20 @@
+#include the Numpy linbrary
+import numpy as np
+
+#define the main() function
+def main():
+
+	i = 0
+	x = 119.0
+	
+	for i in range(120):
+		if((i%2)==0):
+			x += 3
+		else:
+			x -= 5
+	s = "%3.2e" % x
+	
+	print(s)
+	
+if __name__ == "__main__":
+	main()
